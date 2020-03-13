a7aac988c79b7ab2ffdfc462dea7ca0947106460
vai1205 <vaibhav.12may95@gmail.com>
2020-03-13 16:27:25 +0530

    list category/categories and remove

--- backEnd/routes/category.js
+++ backEnd/routes/category.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { categoryCreateValidator } = require("../validators/category");
 const { runValidation } = require("../validators/index");
 const { requireSignIn, adminMiddleWare } = require("../controllers/auth");
-const { create } = require("../controllers/category");
+const { create, list, read, remove } = require("../controllers/category");
 
 router.post(
   "/category",
@@ -12,6 +12,14 @@ router.post(
   requireSignIn,
   adminMiddleWare,
   create
+);
+router.get("/categories", list);
+router.get("/category/:slug", read);
+router.delete(
+  "/category/:slug",
+  requireSignIn,
+  adminMiddleWare,
+  remove
 );
 
 module.exports = router;
